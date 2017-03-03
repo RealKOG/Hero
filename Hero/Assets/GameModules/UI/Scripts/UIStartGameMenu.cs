507109a960c0f2a31d1c1e000b3e9c5ecd572dce
m294955408 <294955408@qq.com>
2017-03-03 21:43:29 +0800

    #19、#18

---
+++ Hero/Assets/GameModules/UI/Scripts/UIStartGameMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStartGameMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartNewGame()
+    {
+        UnityEditor.EditorUtility.DisplayDialog("开始游戏", "开始新游戏", "确定");
+    }
+
+    public void ContinueGame()
+    {
+        UnityEditor.EditorUtility.DisplayDialog("继续游戏", "继续游戏", "确定");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
